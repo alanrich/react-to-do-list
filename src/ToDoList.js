c21acc8249fa0b48389bfdba1b76044128b36285
alanrich-dev <alan.a.rich@gmail.com>
2021-06-17 11:02:56 +0200

    first commit

---
+++ src/ToDoList.js
@@ -0,0 +1,37 @@
+import React, { Component } from  'react';
+
+class ToDoList extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      items: [],
+    }
+    this.addItem = this.addItem.bind.this;
+    this._inputElement = React.createRef();
+  }
+// App Methods
+  addItem(e){
+
+  }
+
+
+  render() {
+    return (
+      <div className='todoListMain'>
+        <div className="header">
+        <form onSubmit={this.addItem}>
+          <input
+            /*ref assigns the value of the input element to a variable
+            providing access to the content of the form */
+            ref={(a)=>this._inputElement = a}
+            type="text"
+            placeholder='enter task'/>
+          <button type='submit'>add</button>
+        </form>
+        </div>
+      </div>
+    );
+  }
+
+}
+export default ToDoList;
