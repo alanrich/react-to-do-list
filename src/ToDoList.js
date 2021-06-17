313ad120bcc622a9a1e735b38df10aa8b0f485fe
alanrich-dev <alan.a.rich@gmail.com>
2021-06-17 16:31:40 +0200

    added logic to add new to do items

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,19 +1,38 @@
 import React, { Component } from  'react';
-import ToDoItems from 'ToDoItems';
+//import ToDoItems from 'ToDoItems';
 
 class ToDoList extends Component {
   constructor(props){
     super(props);
     this.state={
       items: [],
-    }
+    };
     this.addItem = this.addItem.bind.this;
-    this._inputElement = React.createRef();
-  }
+  
+  };
 // App Methods
   addItem(e){
-
-  }
+  // check that the input value exists
+    if (this._inputElement.value !==''){
+  // create a todo item with a unique identifier
+      let newItem = {
+        text: this._inputElement.value,
+        key: Date.now()
+      };
+  // update the state with the new item
+      this.setState((prevState) => {
+  // concatenate the new item to the items key in the state object
+        return {
+          items: prevState.items.concat(newItem)
+        }
+      });
+  // reset the input field
+      this._inputElement.value = '';
+    };
+  // prevent page reload on form submission
+    e.preventDefault();
+    console.log(this.state.items);
+  };
 
 
   render() {
@@ -28,7 +47,7 @@ class ToDoList extends Component {
             type="text"
             placeholder='enter task'/>
           <button type='submit'>add</button>
-          <TodoItems />
+
         </form>
         </div>
       </div>
